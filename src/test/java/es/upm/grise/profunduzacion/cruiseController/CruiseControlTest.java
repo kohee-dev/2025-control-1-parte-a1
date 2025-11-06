afa483818cda0a8292fa3f0f903d092e3119670e
kohee-dev <abartolome215@gmail.com>
2025-11-06 17:51:12 +0100

    Added unit test to CruiseControl

--- src/test/java/es/upm/grise/profunduzacion/cruiseController/CruiseControlTest.java
+++ src/test/java/es/upm/grise/profunduzacion/cruiseController/CruiseControlTest.java
@@ -1,5 +1,36 @@
 package es.upm.grise.profunduzacion.cruiseController;
 
+import es.upm.grise.profundizacion.cruiseControl.CruiseControl;
+import es.upm.grise.profundizacion.cruiseControl.IncorrectSpeedSetException;
+import es.upm.grise.profundizacion.cruiseControl.SpeedSetAboveSpeedLimitException;
+import es.upm.grise.profundizacion.cruiseControl.Speedometer;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class CruiseControlTest {
-	
+
+    private Speedometer speedometer = new Speedometer() {
+        @Override
+        public int getCurrentSpeed() {return 0;}
+    };
+
+    @Test
+    void setSpeedIncorrectValueTest() throws IncorrectSpeedSetException {
+        CruiseControl cruiseControl = new CruiseControl(speedometer);
+        assertThrows(IncorrectSpeedSetException.class, () -> {cruiseControl.setSpeedSet(-1);});
+    }
+
+    @Test
+    void setSpeedAboveSpeedLimitTest() throws SpeedSetAboveSpeedLimitException {
+        CruiseControl cruiseControl = new CruiseControl(speedometer);
+        cruiseControl.setSpeedLimit(5); //Set speed limit
+        assertThrows(SpeedSetAboveSpeedLimitException.class, () -> {cruiseControl.setSpeedSet(6);}); //one above limit
+    }
+
+    @Test
+    void correctSpeedSetTest() throws SpeedSetAboveSpeedLimitException, IncorrectSpeedSetException {
+        CruiseControl cruiseControl = new CruiseControl(speedometer);
+        cruiseControl.setSpeedSet(0);
+        assertEquals(0, cruiseControl.getSpeedSet());
+    }
 }
